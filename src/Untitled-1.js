9d074d1b8b71e2b49c3e92638ee75ba3d665c9a2
Alan Kajara <akajara@gmail.com>
2020-07-27 13:46:11 +0300

    Remove unnecessary File

--- src/Untitled-1.js
+++
@@ -1,8 +0,0 @@
-let tasks = this.state.tasks.toString().toLocaleLowerCase();
-      if (tasks.includes(newTask.toLowerCase()) === false) {
-        console.log(this.state.tasks)
-        this.state.tasks.push(newTask)
-        console.log(this.state.tasks)
-      }else{
-        this.setState({exist: 'Yes'})
-      }
